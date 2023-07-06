c45ba6056b41847c2a502e52c97f8472faabc7a0
Mario <mariosantisteban23@gmail.com>
2023-07-05 21:30:53 -0500

    Vista de empleados v1.2

--- entorno/restaurante/restaurante/urls.py
+++ entorno/restaurante/restaurante/urls.py
@@ -15,17 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from brasas.views import vista_empleado, cambiar_estado, detalle_pedido
+from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    path('empleado/', vista_empleado, name='vista_empleado'),
-    
-    path('cambiar_estado/<int:pedido_id>/', cambiar_estado, name='cambiar_estado'),
-    
-    path('detalle_pedido/<int:pedido_id>/', detalle_pedido, name='detalle_pedido'),
-
+    path('',include('brasas.urls')),
 ]
 
 
